116fd5cfd1a8be188ed9244cf65b238577a51eee
gacarneirojr <gacarneirojr@hotmail.com>
2021-11-04 13:42:02 -0300

    change app folders structure

--- weather-app/index.js
+++ weather-app/index.js
@@ -5,15 +5,24 @@ const PORT = 4000;
 const app = express();
 const __dirname = path.resolve();
 
+app.set('view engine', 'ejs')
+
+app.set('views', path.join(__dirname, './src/views'))
+
+app.get('/weather', (req, res) => {
+    res.render('weather');
+})
+
+
 // This get route will display the static page that will display the input field that will receive the name
 // of the city.
-app.get('/weather', (req, res) => {
-  const options = {
-    root: path.join(__dirname)
-};
-  const fileName = 'index.html'
-  res.sendFile(fileName, options)
-});
+// app.get('/weather', (req, res) => {
+//   const options = {
+//     root: path.join(__dirname)
+// };
+//   const fileName = 'index.html'
+//   res.sendFile(fileName, options)
+// });
 
 import weatherRouter from './src/routes/weather.js';
 
